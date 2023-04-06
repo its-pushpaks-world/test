2b8d000e652c12201fb20b74b97c1358b46d055d
its-pushpaks-world <115705491+its-pushpaks-world@users.noreply.github.com>
2023-04-06 20:02:55 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -16,28 +16,28 @@ data "aws_ami" "linux" {
   owners = ["amazon"]
 }
 
-resource "aws_launch_template" "web" {
-  name_prefix            = "web"
-  image_id               = data.aws_ami.linux.id
-  instance_type          = var.web_instance_type
-  vpc_security_group_ids = [var.web_sg]
-  user_data              = filebase64("install_apache.sh")
+#resource "aws_launch_template" "web" {
+ # name_prefix            = "web"
+  #image_id               = data.aws_ami.linux.id
+ # instance_type          = var.web_instance_type
+  #vpc_security_group_ids = [var.web_sg]
+  #user_data              = filebase64("install_apache.sh")
 
-  tags = {
-    Name = "web"
-  }
-}
+  #tags = {
+   # Name = "web"
+  #}
+#}
 
-resource "aws_autoscaling_group" "web" {
-  name                = "web"
-  vpc_zone_identifier = tolist(var.public_subnet)
-  min_size            = 2
-  max_size            = 3
-  desired_capacity    = 2
+#resource "aws_autoscaling_group" "web" {
+ # name                = "web"
+  #vpc_zone_identifier = tolist(var.public_subnet)
+ # min_size            = 2
+  #max_size            = 3
+  #desired_capacity    = 2
 
-  launch_template {
-    id      = aws_launch_template.web.id
-    version = "$Latest"
-  }
-}
+  #launch_template {
+   # id      = aws_launch_template.web.id
+    #version = "$Latest"
+  #}
+#}
 
